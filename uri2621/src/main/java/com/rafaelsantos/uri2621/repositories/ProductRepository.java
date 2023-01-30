9888e511cca34da6a9dea492e7066268b811c66f
Rafael Santos <rafa.05125@gmail.com>
2023-01-30 18:19:41 -0300

    Repositories

---
+++ uri2621/src/main/java/com/rafaelsantos/uri2621/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.rafaelsantos.uri2621.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafaelsantos.uri2621.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
---
+++ uri2621/src/main/java/com/rafaelsantos/uri2621/repositories/ProviderRepository.java
@@ -0,0 +1,9 @@
+package com.rafaelsantos.uri2621.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rafaelsantos.uri2621.entities.Provider;
+
+public interface ProviderRepository extends JpaRepository<Provider, Long>{
+
+}
