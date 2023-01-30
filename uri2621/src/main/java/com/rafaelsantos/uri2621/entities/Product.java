8f9be977f400c54bad315f7718ebf96ca1ad482f
Rafael Santos <rafa.05125@gmail.com>
2023-01-30 18:17:27 -0300

    Product, Provider

---
+++ uri2621/src/main/java/com/rafaelsantos/uri2621/entities/Product.java
@@ -0,0 +1,92 @@
+package com.rafaelsantos.uri2621.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+	
+	@Id
+	private Long id;
+	private String name;
+	private Integer amount;
+	private Double price;
+		
+	@ManyToOne
+	@JoinColumn(name = "id_providers")
+	private Provider provider;
+	
+	public Product() {}
+
+	public Product(Long id, String name, Integer amount, Double price, Provider provider) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.amount = amount;
+		this.price = price;
+		this.provider = provider;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Provider getProvider() {
+		return provider;
+	}
+
+	public void setProvider(Provider provider) {
+		this.provider = provider;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
+	}
+}
---
+++ uri2621/src/main/java/com/rafaelsantos/uri2621/entities/Provider.java
@@ -0,0 +1,97 @@
+package com.rafaelsantos.uri2621.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "providers")
+public class Provider {
+	
+	@Id
+	private Long id;
+	private String name;
+	private String street;
+	private String city;
+	private String state;
+	
+	@OneToMany(mappedBy = "provider")
+	private List<Product> products = new ArrayList<>();
+	
+	public Provider() {}
+
+	public Provider(Long id, String name, String street, String city, String state) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public List<Product> getProducts() {
+		return products;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Provider other = (Provider) obj;
+		return Objects.equals(id, other.id);
+	}
+}
